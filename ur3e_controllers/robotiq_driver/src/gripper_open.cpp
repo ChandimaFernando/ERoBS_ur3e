ebb44aae44b2dabe332a56da7078a31496dc8968
ChandimaFernando <wfernando1@bnl.gov>
2023-07-17 12:52:07 -0400

    correct gripper configs

---
+++ ur3e_controllers/robotiq_driver/src/gripper_open.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <thread>
+#include "rclcpp/rclcpp.hpp"
+#include "custom_msgs/srv/gripper_cmd.hpp"
+
+#include <chrono>
+#include <cstdlib>
+#include <memory>
+
+using namespace std::chrono_literals;
+
+int main(int argc, char **argv)
+{
+  rclcpp::init(argc, argv);
+
+
+  std::shared_ptr<rclcpp::Node> node = rclcpp::Node::make_shared("gpr_intfc_open");
+
+  rclcpp::Client<custom_msgs::srv::GripperCmd>::SharedPtr client = node->create_client<custom_msgs::srv::GripperCmd>("gripper_service");
+
+  auto request = std::make_shared<custom_msgs::srv::GripperCmd::Request>();
+  request->grip = 10 ;
+  request->cmd = 'O' ;
+
+  while (!client->wait_for_service(1s)) {
+    if (!rclcpp::ok()) {
+      RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Interrupted while waiting for the service. Exiting.");
+      return 0;
+    }
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "service not available, waiting again...");
+  }
+
+  auto result = client->async_send_request(request);
+
+  // Wait for the result.
+  if (rclcpp::spin_until_future_complete(node, result) ==
+    rclcpp::FutureReturnCode::SUCCESS)
+  {
+    RCLCPP_INFO(rclcpp::get_logger("rclcpp"), "Sum: %d", result.get()->status);
+  } else {
+    RCLCPP_ERROR(rclcpp::get_logger("rclcpp"), "Failed to call service add_two_ints");
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}
