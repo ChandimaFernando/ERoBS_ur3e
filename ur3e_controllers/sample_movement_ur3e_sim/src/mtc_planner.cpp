8b16f70f101f2ffffb71cf5a3140d001d46196aa
ChandimaFernando <wfernando1@bnl.gov>
2023-07-25 15:55:41 -0400

    correct z movement

--- ur3e_controllers/sample_movement_ur3e_sim/src/mtc_planner.cpp
+++ ur3e_controllers/sample_movement_ur3e_sim/src/mtc_planner.cpp
@@ -463,7 +463,7 @@ void MTCPlanner::underarm_approach(std::string task_name, std::string obj_to_pic
     }
     
     // Add the waypoints, including the final point
-    intrm_pose.position.y = obj_z ;
+    intrm_pose.position.z = obj_z ;
     RCLCPP_INFO(LOGGER, "intrm_pose.position.z  : %f ", intrm_pose.position.z);  
 
     waypoints.push_back(intrm_pose);
